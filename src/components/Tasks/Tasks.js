85860c0f5a61a0aba60c1648025c7c3d88918188
Md Rahi <fhrfhr102030@gmail.com>
2022-09-29 19:27:36 +0600

    blog section design done

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -62,6 +62,35 @@ const Tasks = () => {
                     <Info info={info}></Info>
                 </div>
             </div>
+            <div className='blog-container m-8 bg-cyan-800 p-4 rounded-md text-gray-200'>
+                <h1 className='text-3xl text-center font-bold py-8'>Blogs</h1>
+                <div className='grid grid-cols-1 lg:grid-cols-1 md:grid-cols-2 gap-4'>
+                <div className='bg-cyan-700 p-4 rounded-md'>
+                <h1 className='text-2xl mb-3'>How does React Works?</h1>
+                <p>React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process. In fancy words, React does batch updates. So putting all pieces together, Reconciliation = Render + Diffing occurs in between + Commit.</p>
+                </div>
+        
+                <div className='bg-cyan-700 p-4 rounded-md'>
+                <h1 className='text-2xl mb-3'>What are the difference between props and state in React?</h1>
+                <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</p>
+                <p>Props can be used with state and functional components. State can be used only with the state components/class component</p>
+                <p>Props are read-only. State is both read and write.</p>
+                </div>
+
+                <div className='bg-cyan-700 p-4 rounded-md'>
+                <h1 className='text-2xl mb-3'>What are the usage of useEffect except api data fething?</h1>
+                <p>Some common use cases of useEffect are:
+                <br />
+                1.Add an event listener for a button
+                  <br />
+                2. Data fetching from API when component mounts
+                 <br />
+                3. Perform an action when state or props change
+                 <br />
+                4. Clean up event listeners when the component unmounts</p>
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
